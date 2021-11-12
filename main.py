33bc9f4bcc558a8c351b798f03e688b3447ae280
YuvaTeja01 <yuvas2001@gmail.com>
2021-11-12 12:28:20 +0530

    req added

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,status,HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
+from starlette.responses import Response
 app = FastAPI()
 
+my_posts = []
 class Post(BaseModel):
     title:str
     author:str
@@ -17,9 +20,41 @@ def sendMessage():
 
 @app.get("/posts")
 def getPosts():
-    return {"data":"all posts here"}
+    return {"data":my_posts}
 
-@app.post("/add-posts")
+@app.post("/add-posts",status_code=status.HTTP_201_CREATED)
 def addPosts(post:Post):
-    print(post)
-    return {"title":post.title, "author":post.author}
+    post_dict= post.dict()
+    post_dict["id"]=randrange(0,10000000)
+    my_posts.append(post_dict)
+    return {"data":post_dict}
+
+def retreivePost(id):
+    for p in my_posts:
+        if p["id"]==id:
+            return p
+
+def deletePost(id):
+    for i,p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+
+@app.get("/posts/latest")
+def getLatest():
+    post = my_posts[len(my_posts)-1]
+    return {"data":post}
+
+@app.get("/posts/{id}")
+def getPost(id:int):
+    post = retreivePost(id)
+    if not post:
+        raise HTTPException(404,"not found")
+   
+    return {"post":post}
+    
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delPost(id:int):
+    index = deletePost(id)
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
